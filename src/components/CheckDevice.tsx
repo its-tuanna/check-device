4914a12bc25ff81a600624c7b1e9e0d1e7f7671a
its-tuanna <tuanna@its-global.vn>
2022-06-08 17:50:44 +0700

    get camera's information

--- src/components/CheckDevice.tsx
+++ src/components/CheckDevice.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { requestVideoPermission, getDevices, returnGetDevices } from '../helpers/video'
+import { requestVideoPermission, getDevices, returnGetDevices, returnVideoTracks } from '../helpers/video'
 
 const CheckDevice = () => {
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
+  const [videoTracks, setVideoTracks] = useState<MediaStreamTrack[]>([])
 
   const startCheck = async () => {
-    await requestVideoPermission();
-    console.log('---------------------------------------------------------------------------------------------------------------------------------------------------');
-    getDevices();
+    const videoTracksReturned = await returnVideoTracks();
+    setVideoTracks(videoTracksReturned);
     const devicesReturned = await returnGetDevices();
     setDevices(devicesReturned);
   }
@@ -16,12 +16,17 @@ const CheckDevice = () => {
     startCheck();
   }, [])
 
-
-
   return (
     <div>
       <div>CheckDevice</div>
       <div>{devices.map((device, index) => <p key={index}>{JSON.stringify(device, null, 2)}</p>)}</div>
+      <hr />
+      <hr />
+      <div>{videoTracks.map((videoTrack, index) => <div key={index}><p>Camera {videoTrack.getSettings().deviceId} has facingMode {videoTrack.getSettings().facingMode}</p>
+        <ul>
+          <li>Width: {videoTrack.getSettings().width}</li>
+          <li>Height: {videoTrack.getSettings().height}</li>
+        </ul></div>)}</div>
     </div>
   )
 }
--- src/helpers/video.ts
+++ src/helpers/video.ts
@@ -1,13 +1,31 @@
+const constrain = {
+  video: { facingMode: "user" },
+  audio: true,
+};
+
 export const requestVideoPermission = async () => {
   if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
-    let stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "user" },
-      audio: true,
-    });
+    let stream = await navigator.mediaDevices.getUserMedia(constrain);
     console.log("All tracks:", stream.getTracks());
-    let { width, height } = stream.getVideoTracks()[0].getSettings();
-    console.log("width: " + width + " height: " + height);
+    // let { width, height } = stream.getVideoTracks()[0].getSettings();
+    // console.log("width: " + width + " height: " + height);
   }
+};
+
+export const returnVideoTracks = async () => {
+  if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices) {
+    let stream = await navigator.mediaDevices.getUserMedia(constrain);
+    console.log("All tracks:", stream.getTracks());
+    console.log(
+      "--------------------------------------------------------------------------------------------------------------------------------------"
+    );
+    console.log("All video tracks:", stream.getVideoTracks());
+    console.log(
+      "--------------------------------------------------------------------------------------------------------------------------------------"
+    );
+    return stream.getVideoTracks();
+  }
+  return [];
 };
 
 export async function getDevices() {
@@ -16,5 +34,7 @@ export async function getDevices() {
 }
 
 export async function returnGetDevices() {
-  return navigator.mediaDevices.enumerateDevices();
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  console.log("All devices:", devices);
+  return devices;
 }
